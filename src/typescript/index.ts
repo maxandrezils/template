40e2430ff052eb2db1d1a340da7ba0f853add4d7
Max-Andre <zilsmc@gmail.com>
2019-08-22 12:11:16 +0200

    added materialze.css

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,10 +1,10 @@
 const displayText = () => {
-  const randomChar = '';
+  let randomChar = '';
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
   for (let i = 0; i < 26; i++) {
-    randomChar += alphabet[Math.random() * 26 + 1];
+    randomChar += `${alphabet[parseInt(Math.random()*26)]} `;
   }
-  console.log(randomChar);
+  console.log(randomChar );
 };
 
 displayText();
