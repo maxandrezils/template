5ce844434714d920c79f123cf2174f0b51b05fd7
Max-Andre <zilsmc@gmail.com>
2019-08-22 13:42:01 +0200

    postcss config

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -2,9 +2,10 @@ const displayText = () => {
   let randomChar = '';
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
   for (let i = 0; i < 26; i++) {
-    randomChar += `${alphabet[parseInt(Math.random()*26)]} `;
+    randomChar += `${alphabet[parseInt(Math.random() * 26)]} `;
   }
-  console.log(randomChar );
+  console.log(randomChar);
+  return 0;
 };
 
 displayText();
